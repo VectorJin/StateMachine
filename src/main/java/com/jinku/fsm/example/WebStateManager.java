ae470e599ecb1174828c66053a08de17c8a546a4
lijk <lijk@jiedaibao.com>
2019-12-18 11:42:53 +0800

    添加 WebStateManager

--- src/main/java/com/jinku/fsm/example/WebStateManager.java
+++ src/main/java/com/jinku/fsm/example/WebStateManager.java
@@ -1,4 +1,27 @@
 package com.jinku.fsm.example;
 
-public class WebStateManager {
+import com.jinku.fsm.core.StateManager;
+
+/**
+ * web环境下的状态管理器（数据库保存状态)
+ * 实现逻辑 跟 本地状态管理器一致，不同之处在于 当前状态读取 和 状态更新
+ */
+public class WebStateManager extends StateManager {
+
+    @Override
+    public String managerKey() {
+        return "web_state_manager";
+    }
+
+    @Override
+    public int currentState(String uuid) {
+        // 从 数据库 读取 记录状态
+        return 0;
+    }
+
+    @Override
+    public boolean setState(String uuid, int newState, int expectState) {
+        // 更改 数据库 对应记录的状态，使用乐观锁机制
+        return false;
+    }
 }
